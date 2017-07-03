0259ee39c5fd3f2ccfe0548f380542e483152718
Lennart Karsten <lennart.karsten@haw-hamburg.de>
2017-07-03 14:49:03 +0200

    GeoServer performance test

---
+++ gis-performance-test/Program.cs
@@ -0,0 +1,13 @@
+﻿using GeoServer;
+
+namespace gis_performance_test
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var geoServerPerformance = new GeoServerPerformance();
+            geoServerPerformance.GeoserverTest();
+        }
+    }
+}
